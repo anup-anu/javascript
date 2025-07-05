9de993dde1f0ec33bb7d8a9188e53ab8dc5b7663
Anoop P Biradar <2602anooppb@gmail.com>
2025-07-05 19:49:24 +0000

    comparision operations day 4

---
+++ js/04comparison.js
@@ -0,0 +1,31 @@
+// Comparison Operators in JavaScript
+
+console.log(2>1);//output is=>true  
+console.log(2<1);//output is=>false
+console.log(2>=1);//output is=>true
+console.log(2<=1);//output is=>false
+console.log(2==1);//output is=>false
+console.log(2!=1);//output is=>true
+
+console.log(2===1);//output is=>false
+console.log(2!==1);//output is=>true
+console.log(2=='1');//output is=>false
+console.log(2==='1');//output is=>false
+console.log(2!='1');//output is=>true
+console.log(2!=='1');//output is=>true 
+
+console.log('2' > 1);//output is=>true
+console.log('01' == 1);//output is=>true
+console.log('2' === 2);//output is=>false
+console.log('2' != 2);//output is=>false
+console.log('2' !== 2);//output is=>true
+console.log('2' < 1);//output is=>false
+console.log('2' <= 1);//output is=>false
+console.log('2' >= 1);//output is=>true
+console.log('2' > '1');//output is=>true
+console.log('2' < '1');//output is=>false
+console.log('2' == '1');//output is=>false
+console.log('2' != '1');//output is=>true
+console.log('2' === '1');//output is=>false
+console.log('2' !== '1');//output is=>true
+
