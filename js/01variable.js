6ea7d812e3f9b2400ace73b503ca32fadc352113
Anoop P Biradar <2602anooppb@gmail.com>
2025-07-05 18:47:32 +0000

    2day

--- js/01variable.js
+++ js/01variable.js
@@ -1,12 +1,12 @@
 const accountId = 144553
 let accountEmail = "anooppb@gmail.com" // we can change this we use this largly 
-var accountPassword = "134565"// we cann change this also 
+var accountPassword = "134565"// we cann change this also and we dont use this bcoz it have block scope issue 
 accountCity = "bijapur"
-
+let accountState; // return undefined bcoz we not defined yet
 // accountId = 12563 this is not allowed bcoz accountId is const
 
 accountEmail = "ap@gmail.com"
 accountPassword = "589462"
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
 console.log(accountId)
---
+++ js/02datatype.js
@@ -0,0 +1,19 @@
+"use strict" // treate all js code as newer version
+
+//alert(3+3) // we are using node js this not run
+
+let name = "anoop"
+let age = 20;
+let isloggedIn = false;
+
+//number =>2 to power 53
+// bigint
+//string = ""
+//boolean => true/false
+//null => stand alone value
+//undefined => vale not assigned
+//symbol =>unique
+
+//object
+
+console.log(typeof(age)); // output: give  the type of value => number,string,object,(null is a object) ,undifined
