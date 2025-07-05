8eda69ab8c78278748f924eb4faac55895419100
Anoop P Biradar <2602anooppb@gmail.com>
2025-07-05 18:30:40 +0000

    code changed

---
+++ js/01variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "anooppb@gmail.com" // we can change this we use this largly 
+var accountPassword = "134565"// we cann change this also 
+accountCity = "bijapur"
+
+// accountId = 12563 this is not allowed bcoz accountId is const
+
+accountEmail = "ap@gmail.com"
+accountPassword = "589462"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+console.log(accountId)
